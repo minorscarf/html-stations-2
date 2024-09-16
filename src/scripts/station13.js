939054d8c32f9dca416746ce79b485829ffa750a
minorscarf <minorscarf@gmail.com>
2024-09-16 17:28:39 +0900

    station13までやった

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,17 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if(box.checked){
+    backColor.style.backgroundColor = 'red';
+    console.log('チェック');
+  }
+  else if(!box.checked){
+    backColor.style.backgroundColor = '';
+    console.log('解除');
+  }
 }
+
+let box = document.getElementById('check');
+let backColor = document.getElementById('text');
+box.checked = false;
+
+box.addEventListener('click', changeBackgroundColor);
